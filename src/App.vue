47538a9e1f42125882ad31d9b2b1ecf8a5deeebe
mesh7 <meshram.shubham27@gmail.com>
2021-10-09 18:45:02 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <HomePageComponent />
+  </div>
+</template>
+
+<script>
+import HomePageComponent from './components/HomePageComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePageComponent,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+.body {
+    background: #1C5EAC;
+    margin: 0;
+}
+
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px 0;
+  padding: 24px;
+}
+</style>
---
+++ src/components/HomePageComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="homePage">HomePageComponent</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePageComponent',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
