50d6ca2ad742c6babced1005d65612a7a3f70b3c
mesh7 <meshram.shubham27@gmail.com>
2022-01-14 23:59:48 +0530

    added changes for components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <HomePageComponent />
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import HomePageComponent from './components/home/HomePageComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePageComponent,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+.body {
+  background: #1c5eac;
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin: 60px 0;
+  padding: 24px;
 }
 </style>
---
+++ src/components/home/HomePageComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-page">
+  <p>
+  HomePageComponent
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import './HomePageComponent.scss';
+
+@Component
+export default class HomePageComponent extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
