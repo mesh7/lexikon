da07c9650e92a0f2d273d6d7457caf5ecc0b4be3
mesh7 <meshram.shubham27@gmail.com>
2022-01-14 23:06:26 +0530

    added changes for the default template

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,32 @@
 <template>
   <div id="app">
-    <HomePageComponent />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HomePageComponent from './components/home/HomePageComponent.vue';
-
-export default {
-  name: 'App',
-  components: {
-    HomePageComponent,
-  },
-};
-</script>
-
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
-
-.body {
-  background: #fffff;
-  margin: 0;
-}
-
+<style lang="scss">
 #app {
-  font-family: Roboto, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 60px 0;
-  padding: 24px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
