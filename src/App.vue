3aa9bea55ed70cdefbcff003ead5dbcb67aa3a8d
mesh7 <meshram.shubham27@gmail.com>
2022-01-15 23:01:21 +0530

    added changes for styling for landing page

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,27 @@
 <template>
-  <div id="app">
+  <div id="app" class="app" :class="mode">
+  <h1> Hello World! </h1>
     <HomePageComponent />
   </div>
 </template>
 
 <script>
-import HomePageComponent from './components/home/HomePageComponent.vue';
+// import HomePageComponent from './components/home/HomePageComponent.vue';
+import './App.scss';
 
 export default {
   name: 'App',
-  components: {
-    HomePageComponent,
+  data() {
+    return {
+      mode: 'dark',
+    };
   },
+  // components: {
+  //   HomePageComponent,
+  // },
 };
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
-
-.body {
-  background: #1c5eac;
-  margin: 0;
-}
-
-#app {
-  font-family: Roboto, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 60px 0;
-  padding: 24px;
-}
 </style>
