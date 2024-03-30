d3e12021f21c71353502e9e4035bad3573fb445b
Shubham Meshram <meshram.shubham27@gmail.com>
2024-03-30 20:29:51 +0530

    added search field and button

---
+++ app.js
@@ -0,0 +1,5 @@
+const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
+
+const result = document.getElementById("results");
+const button = document.getElementById("search-button")
+
