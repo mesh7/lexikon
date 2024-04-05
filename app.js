760be48d15aabfca96bec8256457c6bc1606bcd1
Shubham Meshram <meshram.shubham27@gmail.com>
2024-04-05 21:08:56 +0530

    added event listener for search button

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
 
 const result = document.getElementById("results");
-const button = document.getElementById("search-button")
+const soundButton = document.getElementById("sound-button");
+const searchButton = document.getElementById("search-button");
+
+//event listener for the button
+searchButton.addEventListener("click", () => {
+    let searchInput = document.getElementById("search-input");
+    console.log('Hello input here');
+    console.log(searchInput);
+});
+
+
 
